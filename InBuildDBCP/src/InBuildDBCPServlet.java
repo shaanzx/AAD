7681414c0dab1611cf6f347d3e01e9fa61662b3f
Shan JAY <152029750+shaanzx@users.noreply.github.com>
2025-01-08 11:22:23 +0530

    Add new Module Application2

--- InBuildDBCP/src/InBuildDBCPServlet.java
+++ InBuildDBCP/src/InBuildDBCPServlet.java
@@ -1,16 +1,41 @@
+import jakarta.annotation.Resource;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 @WebServlet(urlPatterns = "/InBuildDBCP")
 public class InBuildDBCPServlet extends HttpServlet {
+
+    @Resource(name = "java:comp/env/jdbc/pool")
+
+    private DataSource dataSource;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Get Request received");
+        try {
+            Connection connection = dataSource.getConnection();
+            ResultSet resultSet = connection.prepareStatement("SELECT * FROM Customer").executeQuery();
+
+            while(resultSet.next()){
+                String id = resultSet.getString(1);
+                String name = resultSet.getString(2);
+                String address = resultSet.getString(3);
+
+                System.out.println(id + " " + name+" " + address);
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
