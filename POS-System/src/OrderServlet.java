463f9e5c9984c7b84709a5640099348cc23dfc32
Shan JAY <152029750+shaanzx@users.noreply.github.com>
2025-01-08 09:34:04 +0530

    Create new module InBuildDBCP

--- POS-System/src/OrderServlet.java
+++ POS-System/src/OrderServlet.java
@@ -1,7 +1,60 @@
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/order")
 public class OrderServlet extends HttpServlet {
 
+    private DataSource dataSource;
+
+    @Override
+    public void init() throws ServletException {
+        dataSource = (DataSource) getServletContext().getAttribute("dataSource");
+        if (dataSource == null) {
+            throw new ServletException("DataSource is not initialized in context");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+
+        String id = req.getParameter("selectedCustomerId");
+        if (id == null) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().write("{\"error\":\"No customer selected.\"}");
+            return;
+        }
+        try {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM customer WHERE id = ?");
+            preparedStatement.setString(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            JsonObjectBuilder customer = Json.createObjectBuilder();
+            customer.add("id",resultSet.getString(1));
+            customer.add("name", resultSet.getString(2));
+            customer.add("address", resultSet.getString(3));
+
+            resp.getWriter().write(customer.build().toString());
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().write("{\"error\":\"Failed to fetch customers.\"}");
+        }
+
+    }
 }
